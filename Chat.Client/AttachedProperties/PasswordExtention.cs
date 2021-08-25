a717cbdf862c145b34c69d4e924ba6449d4d6993
Oleksii Dytiuk <odyti@softserveinc.com>
2021-08-25 21:25:29 +0300

    Dynamic validation to registration farm was added

---
+++ Chat.Client/AttachedProperties/PasswordExtention.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Chat.Client.AttachedProperties
+{
+    public static class PasswordExtention
+    {
+        private static readonly DependencyProperty PasswordInitializedProperty;
+
+        public static readonly DependencyProperty PasswordProperty;
+
+        static PasswordExtention() 
+        {
+            PasswordInitializedProperty = DependencyProperty.RegisterAttached(
+                name: "PasswordInitialized",
+                propertyType: typeof(bool),
+                ownerType: typeof(PasswordExtention),
+                defaultMetadata: new FrameworkPropertyMetadata(false));
+
+            PasswordProperty = DependencyProperty.RegisterAttached(
+                name: "Password",
+                propertyType: typeof(string),
+                ownerType: typeof(PasswordExtention),
+                defaultMetadata: new FrameworkPropertyMetadata(string.Empty, HandelPasswrodChangedCallback) 
+                {
+                    BindsTwoWayByDefault = true,
+                    DefaultUpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
+                });
+        }
+
+        private static void HandelPasswrodChangedCallback(DependencyObject db, DependencyPropertyChangedEventArgs e) 
+        {
+            PasswordBox password = db as PasswordBox;
+
+            if (password is null)
+            {
+                return;
+            }
+
+            if (!(bool)password.GetValue(PasswordInitializedProperty))
+            {
+                password.SetValue(PasswordInitializedProperty, true);
+
+                password.PasswordChanged += HandelPasswordChanged;
+            }
+        }
+
+        private static void HandelPasswordChanged(object sender, RoutedEventArgs e) =>
+            SetPassword((PasswordBox)sender, ((PasswordBox)sender).Password);
+
+        public static string GetPassword(DependencyObject obj) =>
+            (string)obj.GetValue(PasswordProperty);
+
+        public static void SetPassword(DependencyObject obj, string value) =>
+            obj.SetValue(PasswordProperty, value);
+    }
+}
--- Chat.Client/ViewModel/RegistarationViewModel.cs
+++ Chat.Client/ViewModel/RegistarationViewModel.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using ReactiveValidation;
+﻿using ReactiveValidation;
 using ReactiveValidation.Extensions;
 
 namespace Chat.Client.ViewModel
 {
     public class RegistarationViewModel : ValidatableObject
     {
+        private const int MAX_LENGTH_OF_NAME = 15;
+        private const int MIN_LINGTH_OF_PASSWORD = 6;
+
         private string _name;
         private string _password;
         private string _confirmPassword;
@@ -20,10 +17,10 @@ namespace Chat.Client.ViewModel
             Validator = GetValidator();
         }
 
-        public string Name 
+        public string Name
         {
             get => _name;
-            set 
+            set
             {
                 _name = value;
 
@@ -31,21 +28,22 @@ namespace Chat.Client.ViewModel
             }
         }
 
-        public string Password 
+        public string Password
         {
             get => _password;
-            set 
+            set
             {
                 _password = value;
 
                 OnPropertyChanged();
+                OnPropertyChanged(nameof(ConfirmPassword));
             }
         }
 
         public string ConfirmPassword
         {
             get => _confirmPassword;
-            set 
+            set
             {
                 _confirmPassword = value;
 
@@ -53,13 +51,28 @@ namespace Chat.Client.ViewModel
             }
         }
 
+        private bool ConfirmPasswords(string confirmPassword) =>
+            confirmPassword == Password;
+
         private IObjectValidator GetValidator() 
         {
             ValidationBuilder<RegistarationViewModel> validatorBuilder = new();
 
             validatorBuilder.RuleFor(model => model.Name)
                 .NotEmpty()
-                .WithMessage("Your name cannot be emty");
+                .WithMessage("Your name cannot be emty")
+                .Matches(@"^[A-Za-z0-9]+$")
+                .WithMessage("Name can contain only alphabetic chars and numbers and cannot be longer than 15 chars")
+                .MaxLength(MAX_LENGTH_OF_NAME)
+                .WithMessage("Name cannot be longer than 15 chars");
+
+            validatorBuilder.RuleFor(model => model.Password)
+                .MinLength(MIN_LINGTH_OF_PASSWORD)
+                .WithMessage("Pssword cannot be less than 6 chars");
+
+            validatorBuilder.RuleFor(model => model.ConfirmPassword)
+                .Must(ConfirmPasswords)
+                .WithMessage("Confirm password has to be the same as 'Password'");
 
             return validatorBuilder.Build(this);
         }
